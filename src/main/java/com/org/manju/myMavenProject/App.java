20a623e476a0cd7806140f2df382e58b99ad320c
Naga Manjula, Koutha <cnagamanju@gmail.com>
2019-01-16 14:40:18 -0800

    first commit

---
+++ src/main/java/com/org/manju/myMavenProject/App.java
@@ -0,0 +1,15 @@
+package com.org.manju.myMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "This is my first maven project" );
+
+    }
+}
